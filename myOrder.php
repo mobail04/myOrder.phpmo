f6a4240229a6ce97a062112a090020194ab6d709
melfredl <Kirsanovdrt@mail.ru>
2024-05-20 22:29:29 +0300

    test2
    test2

---
+++ myOrder.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Эх, прокачу!</title>
+</head>
+<body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Эх, прокачу!</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+        <header class = "header">
+            <div class = "logo">
+                <img src="img/2.jpg" alt="Logo" class = "logoimage">
+            </div>
+                </div>
+                <nav class = "navigt">
+                <ul>
+                
+                <li class="navigation"><a href="order.php">Заявка</a></li>
+                <li class="navigation"><a href="myOrder.php">Мои заявки</a></li>
+                    </ul>
+                </nav>
+                <nav class = "navig">
+                <ul>
+                        <li class="navigation"><a href="#">Выйти из аккаунта</a></li>
+                        
+                    </ul>
+                </nav>
+            </div>
+        </header>
+</body>
+<div class = "textH2">
+ <h2>Мои заявки</h2>
+</div>
+<form action="" method="POST">
+                <label for = "login">Ваша выбранная  машина:</label>
+                <div class = "photo">
+        <img src="img/7.png" alt="Logo" class = "images">
+</div>
+<div class = "textH2">
+ <h2>Hundai i30</h2>
+</div>
+
+                <label for = "login">Выбранная дата :</label>
+                <label for = "login">30.12.2023</label>
+                <label for = "login">Статус заявки :</label>
+                <label for = "login">Подтверждено</label>
+                <button type = "submit" ">Отменить заявку</button>
+            </form>
+            
+            
+
+
+
+            <form action="" method="POST">
+                <label for = "login">Ваша выбранная  машина:</label>
+                <div class = "photo">
+        <img src="img/2.jpg" alt="Logo" class = "images">
+</div>
+<div class = "textH2">
+ <h2>BMW 7</h2>
+</div>
+
+                <label for = "login">Выбранная дата :</label>
+                <label for = "login">31.12.2023</label>
+                <label for = "login">Статус заявки :</label>
+                <label for = "login">Отказано</label>
+                <button type = "submit" ">Оставить новую заявку</button>
+            </form>
+
+           
+            <div class = "main">
+                
+        <img src="img/3.jfif" alt="Logo" class = "imageS">
+</div>
+<footer class = "footer">
+        <div class = "logo">
+            <img src="img/1.jpg" alt="Logo" class = "logoimage">
+        </div>
+        <nav class = "navig">
+            <span>Заявку можно оставлять 24/7</span>
+            <br>
+            <span></span>
+        </nav>
+        <div class="contacts">
+            <p>Phone:89189037654</p>
+            <p>Email:procat!@mail.ru</p>
+        </div>
+    </footer>
+</html>
+</body>
+</html>
